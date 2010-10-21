9ef25000def0a95d73146d7337386dd0dc231ecf
plusjade <plusjade@gmail.com>
2010-10-21 02:41:34 -0700

    versioned api

--- template.rb
+++ template.rb
@@ -4,11 +4,13 @@ require 'erb'
 
 before do
   @api_site = "http://api.pluspanda.com"
+  @api_site_versioned = "http://api.pluspanda.com/v1"
+  
   if !params[:apikey].nil?
     # redirect to new api method.
     # TODO: log referrer so we know who's still using the old api.
     content_type "text/javascript"
-    redirect @api_site + "/testimonials/widget.js?" + request.query_string
+    redirect @api_site_versioned + "/testimonials/widget.js?" + request.query_string
   end
   
   @links = ['home', 'pricing', 'faq', 'contact', 'activity']
@@ -21,17 +23,17 @@ end
   # http://pluspanda.com/testimonials/add/cI7eHawz
   # http://localhost:3000/testimonials/new?apikey=577ed4ada1f60e44
 get '/testimonials/add/:apikey' do
-  redirect @api_site + "/testimonials/new?apikey=" + params[:apikey]
+  redirect @api_site_versioned + "/testimonials/new?apikey=" + params[:apikey]
 end
 
 ['/', '/home'].each do |path|
   get path do
-    @meta   = 'Embedable customer testimonials and reviews for your business website.'
-    @title  = 'Easily Collect, Manage, and Display Customer Testimonials On Your Website'
-    @active = 'home'
-    @content = erb :index
-    @widget_url = "http://localhost:3000/testimonials/widget.js?apikey=c1b0262bae41a328"
+    @meta       = 'Embedable customer testimonials and reviews for your business website.'
+    @title      = 'Easily Collect, Manage, and Display Customer Testimonials On Your Website'
+    @active     = 'home'
+    @widget_url = @api_site_versioned + "/testimonials/widget.js?apikey=nbE6zpH"
     @embed_code = "fill this in later"
+    @content    = erb :index
     erb :template
   end
 end
@@ -75,6 +77,32 @@ get '/activity' do
   @github = JSON.parse(res.body)
 
   @content = erb :activity
+  erb :template
+end
+
+get '/test' do
+  @meta   = ''
+  @title  = 'Test APi =)'
+
+  require 'net/http'
+  require 'uri'
+  require 'json'
+  
+  local_url = 'http://localhost:3000/testimonials.json?apikey=79ccf4bb93bd623b'
+  local_get = '/testimonials.json?apikey=79ccf4bb93bd623b'
+  production_url = @api_site_versioned + '/testimonials.json?apikey=nbE6zpH' 
+  production_get = '/testimonials.json?apikey=nbE6zpH'
+  
+  
+  url = URI.parse(production_url)
+     res = Net::HTTP.start(url.host, url.port) {|http|
+       http.get(production_get)
+     }
+
+  data          = JSON.parse(res.body)
+  @testimonials = data["testimonials"]
+  @data         = data["update_data"]
+  @content      = erb :test
   erb :template
 end
 
